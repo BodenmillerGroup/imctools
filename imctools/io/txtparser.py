31229c1431d332ade54a86bc810bb494a00487e8
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2018-11-22 21:10:19 +0100

    Sergio: merged txtparser

--- imctools/io/txtparser.py
+++ imctools/io/txtparser.py
@@ -5,7 +5,15 @@ from imctools.io.imcacquisition import ImcAcquisition
 from imctools.io.txtparserbase import TxtParserBase
 from imctools.io.abstractparser import AbstractParser
 import array
-import numpy as np
+
+try:
+    import numpy as np
+    _have_numpy = True
+except ImportError as ix:
+    _have_numpy = False
+
+TXT_FILENDING='.txt'
+
 
 class TxtParser(AbstractParser, TxtParserBase):
     """
@@ -14,23 +22,44 @@ class TxtParser(AbstractParser, TxtParserBase):
 
     def __init__(self, filename, filehandle=None):
         AbstractParser.__init__(self)
-        TxtParserBase.__init__(self, filename, filehandle=None)
-        self.data = np.array(self.data)
+        self.filename = filename
+        if filehandle is None:
+            with open(filename, 'r') as txtfile:
+                self.parse_csv3(txtfile)
+        else:
+            filehandle.seek(0)
+            self.parse_csv3(filehandle)
+            self.origin ='txt'
+            self.channel_labels = self.channel_metals[:]
+            self.channel_metals[3:] = self.clean_channel_metals(self.channel_metals[3:])
+        if _have_numpy:
+            self.data = np.array(self.data)
+
+    @property
+    def ac_id(self):
+        ac_id = self._txtfn_to_ac(self.filename)
+        return ac_id
+
+    @staticmethod
+    def _txtfn_to_ac(fn):
+        return fn.rstrip(TXT_FILENDING).split('_')[-1]
 
     def get_imc_acquisition(self):
         """
         Returns the imc acquisition object
         :return:
         """
-        img = self._reshape_long_2_cyx(self.data, is_sorted=True)
+        dat = self.data
+        img = self._reshape_long_2_cyx(dat, is_sorted=True)
         ac_id = self.ac_id
+
         return ImcAcquisition(ac_id, self.filename,
-                                  img,
-                                  self.channel_metals,
-                                  self.channel_labels,
-                                  original_metadata=None,
-                                  image_description=None,
-                                  origin=self.origin,
+                              img,
+                              self.channel_metals,
+                              self.channel_labels,
+                              original_metadata=None,
+                              image_description=None,
+                              origin=self.origin,
                               offset=3)
 
     @staticmethod
@@ -39,6 +68,7 @@ class TxtParser(AbstractParser, TxtParserBase):
         clean the names to be nice
         :return:
         """
+        print(names)
         # find which version it is
         names = [n.strip("\r") for n in names]
         names = [n.strip("\n") for n in names]
@@ -48,6 +78,7 @@ class TxtParser(AbstractParser, TxtParserBase):
             #get m123di
 
             names = [n[(n.rfind('(')+1):(n.rfind(')'))] for n in names]
+            print(names)
 
         # string of sort aasbas_mn123
         elif '_' in names[0]:
@@ -65,19 +96,45 @@ class TxtParser(AbstractParser, TxtParserBase):
 
         return names
 
+    def parse_csv(self, txtfile, first_col=3):
+        txtreader = csv.reader(txtfile, delimiter='\t')
+        header = txtreader.next()
+        channel_names = header[first_col:]
+        nchan = len(channel_names)
+        txtreader = csv.reader(txtfile, delimiter='\t',
+                               quoting=csv.QUOTE_NONNUMERIC)
+        txtreader.next()
+        data = list()
+        for row in txtreader:
+            rowar = array.array('f')
+            rowar.fromlist(row[first_col:])
+            data.append(rowar)
+        self.data = data
+        self.channel_metals = channel_names
+
+    def parse_csv2(self, txtfile, first_col=3):
+        header = txtfile.readline().split('\t')
+        channel_names = header[first_col:]
+        data = [[float(v) for v in row.split('\t')[first_col:]] for row in txtfile]
+        self.data = data
+        self.channel_metals = channel_names
 
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    import timeit
-    fn = '/home/vitoz/temp/HIER_healthy_4_3_HIER5_4.txt'
-    #fn = '/mnt/imls-bod/data_vito/Spheres/20160330_BigInspheroIMC2/20150330_IS2335_5um_3_site1_ac2_200hz_2200x2200/20150330_IS2335_5um_3_site1_ac2_200hz_2200x2200.txt'
-    #fn = '/home/vitoz/temp/20150330_IS2335_5um_3_site1_ac2_200hz_2200x2200.txt'
-    tic = timeit.default_timer()
-    imc_txt = TxtParser(fn)
-    tic = timeit.default_timer();
-    imc_ac = imc_txt.get_imc_acquisition()
-    #iw = imc_ac.get_image_writer('/home/vitoz/temp/test_iridium.ome.tiff', mass=['191', '193'])
-    #iw.save_image(mode='ome')
-    #iw = imc_ac.get_image_writer('/home/vitoz/temp/HIER_healthy_4_3_HIER5_4.ome.tiff')
-    #iw.save_image(mode='ome')
+    def parse_csv3(self, txtfile, first_col=3):
+        """
+        The fastest  csv parser so far
+        :param filename:
+        :param first_col: First column to consider
+        :return:
+        """
+        header = txtfile.readline().split('\t')
+        channel_names = header[first_col:]
+        nchan = len(channel_names)
+        rowar = array.array('f')
+        for row in txtfile:
+            for v in row.split('\t')[first_col:]:
+                rowar.append(float(v))
+        nrow = int(len(rowar)/nchan)
+        data = [rowar[(i*nchan):(i*nchan+nchan)] for i in range(nrow)]
+        self.data = data
+        self.channel_metals = channel_names
