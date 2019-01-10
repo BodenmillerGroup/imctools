16600fe2ecb92391eeff3fbff8e42e28c6214a88
Anton Rau <anton.rau@gmail.com>
2019-01-10 17:03:14 +0100

    Added missing .nojekyll file Disable pytest-cov plugin due to incompatibility with Python 2.x

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from setuptools import setup, find_packages
 
-
 with open('README.md') as f:
     readme = f.read()
 
@@ -19,8 +18,7 @@ setup(
     url='https://github.com/bodenmillerlab/imctools',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires = [
-                       'tifffile>=0.13.5', 'scikit-image', 'numpy', 'scipy',
-                        'pandas' ]
+    install_requires=[
+        'tifffile>=0.13.5', 'scikit-image', 'numpy', 'scipy',
+        'pandas']
 )
-
