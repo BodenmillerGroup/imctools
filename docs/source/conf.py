d22edb60c1a3789f0c400cc8faada1bb33e008dd
Anton Rau <anton.rau@gmail.com>
2019-06-27 16:37:35 +0200

    Change version number to 1.0.0 for the first public PyPI release.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Vito Zanotelli'
 author = 'Vito Zanotelli'
 
 # The short X.Y version
-version = '0.2.1'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
