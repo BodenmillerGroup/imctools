aa629dda33b075676d51dd1f51b596b1520f9486
Anton Rau <anton.rau@gmail.com>
2019-06-28 11:01:11 +0200

    Switch from "guzzle" to "classic" Sphinx theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,13 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import guzzle_sphinx_theme
-
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
-
-# Register the theme as an extension to generate a sitemap.xml
-extensions.append("guzzle_sphinx_theme")
+html_theme = "classic"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
