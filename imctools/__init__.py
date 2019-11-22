c0150783dc9a5cd2f94c18e898b29ad2490906fa
Anton Rau <anton.rau@gmail.com>
2019-11-22 15:38:35 +0100

    Change branch version to 2.0.0 as a starting point.

--- imctools/__init__.py
+++ imctools/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for imctools."""
 
 __author__ = """Vito Zanotelli, Anton Rau"""
-__email__ = 'vito.zanotelli@uzh.ch, anton.rau@uzh.ch'
-__version__ = '0.0.1'
+__email__ = "vito.zanotelli@uzh.ch, anton.rau@uzh.ch"
+__version__ = "2.0.0"
--- imctools/cli.py
+++ imctools/cli.py
@@ -8,8 +8,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for imctools."""
-    click.echo("Replace this message by putting your code into "
-               "imctools.cli.main")
+    click.echo("Replace this message by putting your code into " "imctools.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
