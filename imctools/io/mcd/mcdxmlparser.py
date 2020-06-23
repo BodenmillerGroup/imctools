85f63d4d41204be8c8f0daaf781ea5358893f634
Anton Rau <anton.rau@gmail.com>
2020-06-23 12:33:54 +0200

    Manage cases when MCD files have no acquisitions.

--- imctools/io/mcd/mcdxmlparser.py
+++ imctools/io/mcd/mcdxmlparser.py
@@ -90,80 +90,83 @@ class McdXmlParser:
             session.panoramas[panorama.id] = panorama
 
         rois = dict()
-        for r in self.metadata.get(const.ACQUISITION_ROI):
-            rois[int(r.get(const.ID))] = r
+        if const.ACQUISITION_ROI in self.metadata:
+            for r in self.metadata.get(const.ACQUISITION_ROI):
+                rois[int(r.get(const.ID))] = r
 
-        for a in self.metadata.get(const.ACQUISITION):
-            roi = rois.get(int(a.get(const.ACQUISITION_ROI_ID)))
-            panorama = session.panoramas.get(int(roi.get(const.PANORAMA_ID)))
-            slide_id = panorama.slide_id
-            slide = session.slides.get(slide_id)
+        if const.ACQUISITION in self.metadata:
+            for a in self.metadata.get(const.ACQUISITION):
+                roi = rois.get(int(a.get(const.ACQUISITION_ROI_ID)))
+                panorama = session.panoramas.get(int(roi.get(const.PANORAMA_ID)))
+                slide_id = panorama.slide_id
+                slide = session.slides.get(slide_id)
 
-            has_before_ablation_image = (
-                int(a.get(const.BEFORE_ABLATION_IMAGE_END_OFFSET, 0))
-                - int(a.get(const.BEFORE_ABLATION_IMAGE_START_OFFSET, 0))
-            ) > 0
-            has_after_ablation_image = (
-                int(a.get(const.AFTER_ABLATION_IMAGE_END_OFFSET, 0))
-                - int(a.get(const.AFTER_ABLATION_IMAGE_START_OFFSET, 0))
-            ) > 0
+                has_before_ablation_image = (
+                    int(a.get(const.BEFORE_ABLATION_IMAGE_END_OFFSET, 0))
+                    - int(a.get(const.BEFORE_ABLATION_IMAGE_START_OFFSET, 0))
+                ) > 0
+                has_after_ablation_image = (
+                    int(a.get(const.AFTER_ABLATION_IMAGE_END_OFFSET, 0))
+                    - int(a.get(const.AFTER_ABLATION_IMAGE_START_OFFSET, 0))
+                ) > 0
 
-            # TODO: check if the issue is fixed in newer Fluidigm versions
-            roi_start_x_pos_um = float(a.get(const.ROI_START_X_POS_UM, 0))
-            roi_start_y_pos_um = float(a.get(const.ROI_START_Y_POS_UM, 0))
-            if slide.sw_version is None or version.parse(slide.sw_version) <= version.parse("7.0.5189.0"):
-                roi_start_x_pos_um /= 1000
-                roi_start_y_pos_um /= 1000
+                # TODO: check if the issue is fixed in newer Fluidigm versions
+                roi_start_x_pos_um = float(a.get(const.ROI_START_X_POS_UM, 0))
+                roi_start_y_pos_um = float(a.get(const.ROI_START_Y_POS_UM, 0))
+                if slide.sw_version is None or version.parse(slide.sw_version) <= version.parse("7.0.5189.0"):
+                    roi_start_x_pos_um /= 1000
+                    roi_start_y_pos_um /= 1000
 
-            acquisition = Acquisition(
-                slide_id,
-                int(a.get(const.ID)),
-                self.origin,
-                source_path,
-                int(a.get(const.MAX_X)),
-                int(a.get(const.MAX_Y)),
-                signal_type=a.get(const.SIGNAL_TYPE, "Dual"),
-                segment_data_format=a.get(const.SEGMENT_DATA_FORMAT, "Float"),
-                ablation_frequency=float(a.get(const.ABLATION_FREQUENCY, 100)),
-                ablation_power=float(a.get(const.ABLATION_POWER, 0)),
-                start_timestamp=parse(a.get(const.START_TIME_STAMP, session.created.isoformat())),
-                end_timestamp=parse(a.get(const.END_TIME_STAMP, session.created.isoformat())),
-                movement_type=a.get(const.MOVEMENT_TYPE, "XRaster"),
-                ablation_distance_between_shots_x=float(a.get(const.ABLATION_DISTANCE_BETWEEN_SHOTS_X, 1)),
-                ablation_distance_between_shots_y=float(a.get(const.ABLATION_DISTANCE_BETWEEN_SHOTS_Y, 1)),
-                template=a.get(const.TEMPLATE, ""),
-                roi_start_x_pos_um=roi_start_x_pos_um,
-                roi_start_y_pos_um=roi_start_y_pos_um,
-                roi_end_x_pos_um=float(a.get(const.ROI_END_X_POS_UM, 0)),
-                roi_end_y_pos_um=float(a.get(const.ROI_END_Y_POS_UM, 0)),
-                description=a.get(const.DESCRIPTION, "ROI"),
-                metadata=dict(a),
-                has_before_ablation_image=has_before_ablation_image,
-                has_after_ablation_image=has_after_ablation_image,
-            )
-            slide = session.slides.get(acquisition.slide_id)
-            acquisition.slide = slide
-            slide.acquisitions[acquisition.id] = acquisition
-            session.acquisitions[acquisition.id] = acquisition
+                acquisition = Acquisition(
+                    slide_id,
+                    int(a.get(const.ID)),
+                    self.origin,
+                    source_path,
+                    int(a.get(const.MAX_X)),
+                    int(a.get(const.MAX_Y)),
+                    signal_type=a.get(const.SIGNAL_TYPE, "Dual"),
+                    segment_data_format=a.get(const.SEGMENT_DATA_FORMAT, "Float"),
+                    ablation_frequency=float(a.get(const.ABLATION_FREQUENCY, 100)),
+                    ablation_power=float(a.get(const.ABLATION_POWER, 0)),
+                    start_timestamp=parse(a.get(const.START_TIME_STAMP, session.created.isoformat())),
+                    end_timestamp=parse(a.get(const.END_TIME_STAMP, session.created.isoformat())),
+                    movement_type=a.get(const.MOVEMENT_TYPE, "XRaster"),
+                    ablation_distance_between_shots_x=float(a.get(const.ABLATION_DISTANCE_BETWEEN_SHOTS_X, 1)),
+                    ablation_distance_between_shots_y=float(a.get(const.ABLATION_DISTANCE_BETWEEN_SHOTS_Y, 1)),
+                    template=a.get(const.TEMPLATE, ""),
+                    roi_start_x_pos_um=roi_start_x_pos_um,
+                    roi_start_y_pos_um=roi_start_y_pos_um,
+                    roi_end_x_pos_um=float(a.get(const.ROI_END_X_POS_UM, 0)),
+                    roi_end_y_pos_um=float(a.get(const.ROI_END_Y_POS_UM, 0)),
+                    description=a.get(const.DESCRIPTION, "ROI"),
+                    metadata=dict(a),
+                    has_before_ablation_image=has_before_ablation_image,
+                    has_after_ablation_image=has_after_ablation_image,
+                )
+                slide = session.slides.get(acquisition.slide_id)
+                acquisition.slide = slide
+                slide.acquisitions[acquisition.id] = acquisition
+                session.acquisitions[acquisition.id] = acquisition
 
-        for c in self.metadata.get(const.ACQUISITION_CHANNEL):
-            if c.get(const.CHANNEL_NAME) in ("X", "Y", "Z"):
-                continue
-            name = c.get(const.CHANNEL_NAME).replace("(", "").replace(")", "").strip()
-            mass = "".join([m for m in name if m.isdigit()])
-            channel = Channel(
-                int(c.get(const.ACQUISITION_ID)),
-                int(c.get(const.ID)),
-                int(c.get(const.ORDER_NUMBER)),
-                name,
-                label=c.get(const.CHANNEL_LABEL),
-                mass=int(mass) if mass != "" else None,
-                metadata=dict(c),
-            )
-            session.channels[channel.id] = channel
-            ac = session.acquisitions.get(channel.acquisition_id)
-            channel.acquisition = ac
-            ac.channels[channel.id] = channel
+        if const.ACQUISITION_CHANNEL in self.metadata:
+            for c in self.metadata.get(const.ACQUISITION_CHANNEL):
+                if c.get(const.CHANNEL_NAME) in ("X", "Y", "Z"):
+                    continue
+                name = c.get(const.CHANNEL_NAME).replace("(", "").replace(")", "").strip()
+                mass = "".join([m for m in name if m.isdigit()])
+                channel = Channel(
+                    int(c.get(const.ACQUISITION_ID)),
+                    int(c.get(const.ID)),
+                    int(c.get(const.ORDER_NUMBER)),
+                    name,
+                    label=c.get(const.CHANNEL_LABEL),
+                    mass=int(mass) if mass != "" else None,
+                    metadata=dict(c),
+                )
+                session.channels[channel.id] = channel
+                ac = session.acquisitions.get(channel.acquisition_id)
+                channel.acquisition = ac
+                ac.channels[channel.id] = channel
 
         self._session = session
 
